bfe232d68b5a03b34b3a41d3e995bf83498398c7
zhuzhu333 <czg1997418>
2019-10-21 20:17:49 +0800

    fadfdfdaf

---
+++ consumer/src/main/java/com/kgc/consumer/model/GroupInfo.java
@@ -0,0 +1,26 @@
+package com.kgc.consumer.model;
+
+/**
+ * Created By: YunCeng
+ * Created on: 2019/10/21:19:22
+ */
+public class GroupInfo {
+    private String groupName;
+    private String url;
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
---
+++ consumer/src/main/java/com/kgc/consumer/vo/GoodGroupVo.java
@@ -0,0 +1,19 @@
+package com.kgc.consumer.vo;
+
+import com.kgc.consumer.model.GroupInfo;
+
+/**
+ * Created By: YunCeng
+ * Created on: 2019/10/21:19:17
+ */
+public class GoodGroupVo {
+
+
+    private GroupInfo groupInfo;
+
+
+
+
+
+
+}
