ea34ac96a1bd2b71467a82f7b3a3cd14d437e659
zhuzhu333 <czg1997418>
2019-10-21 20:14:05 +0800

    getUrl

--- consumer/src/main/java/com/kgc/consumer/controller/indexController.java
+++ consumer/src/main/java/com/kgc/consumer/controller/indexController.java
@@ -6,6 +6,7 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.kgc.consumer.contants.GoodContant;
+import com.kgc.consumer.model.GroupInfo;
 import com.kgc.consumer.utils.GetClientIpAddr;
 import com.kgc.consumer.utils.RedisUtils;
 import com.kgc.consumer.utils.result.ReturnResult;
@@ -147,6 +148,31 @@ public class indexController {
         });
         return ReturnResultUtils.returnSuccess(goodsVoList);
     }
+
+
+    @ApiOperation("获取Url")
+    @GetMapping(value = "/getGroupInfo")
+    public GroupInfo getGroupInfo(@ApiParam(value = "groupName", required = true) @RequestParam(value = "groupName", required = true)String groupName){
+
+
+        String groupSort=indexService.selectByName(groupName).getGoodSort();
+
+        GroupInfo groupInfo=new GroupInfo();
+
+        String urll="http://you.163.com/item/list?categoryId=";
+
+        groupInfo.setUrl(urll+groupSort);
+        groupInfo.setGroupName(groupName);
+
+
+
+        return groupInfo;
+
+
+    }
+
+
+
 
 }
 
